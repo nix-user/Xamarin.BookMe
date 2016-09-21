550ba7b8818430cccf7e19096a9740ced9ccbdd6
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-21 17:26:34 +0300

    created unit tests project

---
+++ BookMeMobile/BookMeMobile.UnitTests/ViewModels/LoginViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookMeMobile.ViewModels.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Forms;
+
+namespace BookMeMobile.UnitTests.ViewModels
+{
+    [TestClass]
+    public class LoginViewModelTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+        }
+
+        [TestMethod]
+        public void SignInCommand_Should_Show_Select_Page_If_Operation_Was_Successful()
+        {
+        }
+    }
+}
