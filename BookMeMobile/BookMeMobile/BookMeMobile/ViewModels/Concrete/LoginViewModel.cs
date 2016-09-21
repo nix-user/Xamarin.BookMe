78e8693eaea6d39cd713307c61dd7c73e5f3ad51
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-21 11:51:13 +0300

    added folder structure and services interfaces

---
+++ BookMeMobile/BookMeMobile/BookMeMobile/ViewModels/Concrete/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMeMobile.ViewModels.Concrete
+{
+    class LoginViewModel : BaseViewModel
+    {
+    }
+}
