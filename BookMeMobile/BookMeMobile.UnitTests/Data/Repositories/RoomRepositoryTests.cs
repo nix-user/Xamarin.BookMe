0a55ec7278e50b1fa21e67e476364ca8d4fe13a9
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-22 18:20:26 +0300

    added tests for GetEmptyRoomMethod

--- BookMeMobile/BookMeMobile.UnitTests/Data/Repositories/RoomRepositoryTests.cs
+++ BookMeMobile/BookMeMobile.UnitTests/Data/Repositories/RoomRepositoryTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BookMeMobile.Data;
 using BookMeMobile.Data.Abstract;
 using BookMeMobile.Entity;
+using BookMeMobile.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -48,6 +49,58 @@ namespace BookMeMobile.UnitTests.Data.Repositories
 
             //assert
             this.httpServiceMock.Verify(m => m.Get<Room>(expectedRoute), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetEmptyRoom_With_LargeRoomParameter_Should_Call_Http_Service_Get_With_Correct_URL()
+        {
+            //arrange
+            RoomRepository roomRepository = new RoomRepository(this.httpServiceMock.Object);
+            RoomFilterParameters filterParameters = new RoomFilterParameters()
+            {
+                IsLarge = true,
+                HasPolycom = false,
+                From = new DateTime(2016, 9, 22, 3, 0, 0),
+                To = new DateTime(2016, 9, 22, 5, 0, 0),
+            };
+
+            string expectedRoute = string.Format(RestURl.GetEmptyRoom,
+                "22-Sep-16 12:00:00 AM",
+                "22-Sep-16 2:00:00 AM",
+                "False",
+                "True");
+
+            //act
+            await roomRepository.GetEmptyRoom(filterParameters);
+
+            //assert
+            this.httpServiceMock.Verify(m => m.Get<IEnumerable<Room>>(expectedRoute), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetEmptyRoom_With_HasPolycomParameter_Should_Call_Http_Service_Get_With_Correct_URL()
+        {
+            //arrange
+            RoomRepository roomRepository = new RoomRepository(this.httpServiceMock.Object);
+            RoomFilterParameters filterParameters = new RoomFilterParameters()
+            {
+                IsLarge = false,
+                HasPolycom = true,
+                From = new DateTime(2016, 9, 22, 3, 0, 0),
+                To = new DateTime(2016, 9, 22, 5, 0, 0),
+            };
+
+            string expectedRoute = string.Format(RestURl.GetEmptyRoom,
+                "22-Sep-16 12:00:00 AM",
+                "22-Sep-16 2:00:00 AM",
+                "True",
+                "False");
+
+            //act
+            await roomRepository.GetEmptyRoom(filterParameters);
+
+            //assert
+            this.httpServiceMock.Verify(m => m.Get<IEnumerable<Room>>(expectedRoute), Times.Once);
         }
     }
 }
