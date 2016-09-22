f3f82f938ce01024a953777977d304adc8e7dbd2
NIX\kudrya <v.a.kudrya@nixsolutions.com>
2016-09-22 16:20:13 +0300

    Refactored Added xml-documentation to some elements

--- BookMeMobile/BookMeMobile/BookMeMobile/BL/Concrete/ReservationService.cs
+++ BookMeMobile/BookMeMobile/BookMeMobile/BL/Concrete/ReservationService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BookMeMobile.BL.Abstract;
+﻿using BookMeMobile.BL.Abstract;
 
 namespace BookMeMobile.BL.Concrete
 {
+    /// <summary>
+    /// This class performs a logic for managing reservations
+    /// </summary>
     internal class ReservationService : BaseService, IReservationService
     {
     }
