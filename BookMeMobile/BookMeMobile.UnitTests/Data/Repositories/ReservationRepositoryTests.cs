a8fedd6fd54fe5bb5dfd886279f3192a47272563
NIX\kudrya <v.a.kudrya@nixsolutions.com>
2016-09-22 17:33:59 +0300

    Added tests for reservation repository

---
+++ BookMeMobile/BookMeMobile.UnitTests/Data/Repositories/ReservationRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using BookMeMobile.Data;
+using BookMeMobile.OperationResults;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BookMeMobile.UnitTests.Data.Repositories
+{
+    [TestClass]
+    public class ReservationRepositoryTests
+    {
+        private ReservationRepository reservationRepository;
+        private Mock<HttpService> httpServcieMock;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.reservationRepository = new ReservationRepository();
+            this.httpServcieMock = new Mock<HttpService>();
+        }
+
+        [TestMethod]
+        public void Remove_DeleteMethodShouldBeCalledOnce()
+        {
+            Task<BaseOperationResult> result = this.reservationRepository.Remove(It.IsAny<int>());
+            result.Wait();
+
+            this.httpServcieMock.Verify(x => x.Delete(It.IsAny<string>()), Times.Once);
+        }
+    }
+}
