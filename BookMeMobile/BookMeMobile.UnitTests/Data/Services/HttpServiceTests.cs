6a15f54da87655510bd2260ca5209abba2b263c1
Sergey Yushkin <sergey.yushkin@nixsolutions.com>
2016-09-23 11:51:50 +0300

    added test for HttpService Get method when HttpHandler Get throws exception

---
+++ BookMeMobile/BookMeMobile.UnitTests/Data/Services/HttpServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using BookMeMobile.BL.Abstract;
+using BookMeMobile.Data;
+using BookMeMobile.Data.Abstract;
+using BookMeMobile.Entity;
+using BookMeMobile.Enums;
+using BookMeMobile.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BookMeMobile.UnitTests.Data.Services
+{
+    [TestClass]
+    public class HttpServiceTests
+    {
+        private Mock<IDependencyService> dependencyServiceMock;
+        private Mock<IHttpHandler> httpHandlerMock;
+        private Mock<IFileWorker> fileWorkerMock;
+        private Mock<HttpHeaders> httpRequestHeadersMock;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.httpHandlerMock = new Mock<IHttpHandler>();
+            this.fileWorkerMock = new Mock<IFileWorker>();
+            this.dependencyServiceMock = new Mock<IDependencyService>();
+            this.httpRequestHeadersMock = new Mock<HttpHeaders>();
+
+            this.dependencyServiceMock.Setup(m => m.Get<IFileWorker>()).Returns(this.fileWorkerMock.Object);
+            this.httpHandlerMock.SetupGet(m => m.RequestHeaders).Returns(this.httpRequestHeadersMock.Object);
+        }
+
+        [TestMethod]
+        public async Task Get_When_Http_Handler_Get_Throws_Exception_Should_Return_Connection_Error()
+        {
+            //arrange
+            this.fileWorkerMock.Setup(m => m.LoadTextAsync()).Returns(Task.FromResult(string.Empty));
+            this.httpHandlerMock.Setup(handler => handler.GetAsync(It.IsAny<string>())).Throws(new Exception());
+            HttpService httpService = new HttpService(this.dependencyServiceMock.Object, this.httpHandlerMock.Object);
+            string testRoute = "testRoute";
+
+            //act
+            var result = await httpService.Get<Room>(testRoute);
+
+            //assert
+            Assert.AreEqual(StatusCode.ConnectionProblem, result.Status);
+        }
+    }
+}
