9a729ad99a9e4a58d58fa2682eeb3e00362538dd
Viktor Kudria <v.a.kudrya@nixsolutions.com>
2016-10-28 10:51:59 +0300

    Changed reservation repository to fake

--- BookMeMobile/BookMeMobile/BookMeMobile/App.cs
+++ BookMeMobile/BookMeMobile/BookMeMobile/App.cs
@@ -66,7 +66,7 @@ namespace BookMeMobile
             App.Container.RegisterType<LoginViewModel>();
             App.Container.RegisterType<ListRoomManager>();
             App.Container.RegisterType<SelectViewModel>();
-            App.Container.RegisterType<IReservationRepository, ReservationRepository>();
+            App.Container.RegisterType<IReservationRepository, FakeReservationRepository>();
             App.Container.RegisterType<IReservationService, ReservationService>();
             App.Container.RegisterType<IProfileService, ProfileService>();
             App.Container.RegisterType<IRoomRepository, RoomRepository>();
